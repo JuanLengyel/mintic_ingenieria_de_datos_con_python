89d6573f70f80b42774ce04edfaabd59c9752375
Juan Sebastian Lengyel <julengyel@personalsoft.com.co>
2019-11-18 16:29:25 -0500

    Add function to fill missing titles

--- newspaper_recipe/newspaper_recipe.py
+++ newspaper_recipe/newspaper_recipe.py
@@ -1,10 +1,17 @@
 import argparse
+import hashlib
 import logging
 logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
 
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
 
+nltk.download('punkt')
+nltk.download('stopwords')
+
+stop_words = set(stopwords.words('spanish'))
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +23,11 @@ def main(filename):
 
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_from_body(df)
+    df['n_token_title'] = _tokenize_column(df, 'title')
+    df['n_token_body'] = _tokenize_column(df, 'body')
 
     return df
 
@@ -41,6 +53,57 @@ def _extract_host(df):
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
     return df
 
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)/[^/]+$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                    )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each row')
+
+    uids = (df
+                .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+                .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+    return df.set_index('uid')
+
+def _remove_new_lines_from_body(df):
+    logger.info('Cleaning new lines from body')
+
+    stripped_body = (df
+                        .apply(lambda row: row['body'], axis=1)
+                        .apply(lambda body: list(body))
+                        .apply(lambda letters: list(map(lambda letter: letter.replace('\n',''), letters)))
+                        .apply(lambda letters: ''.join(letters))
+                    )
+
+    df['body'] = stripped_body
+
+    return df
+
+def _tokenize_column(df, column_name):
+    tokenized_column = (df
+                            .dropna()
+                            .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                            .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))  
+                            .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                            .apply(lambda words_list: list(filter(lambda word: word not in stop_words, words_list)))
+                            .apply(lambda words_list: len(words_list))
+                        )
+
+    return tokenized_column
 
 if __name__ == "__main__":
     
